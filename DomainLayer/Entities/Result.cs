12e2b8f0c65b7e09b462a40dd69d98634c250d78
punkmyduck <punkmyduck@mail.ru>
2025-08-09 15:26:12 +0300

    Импорт моделей из EF, добавление ДТО для CSV, создание контроллера для загрузки файлов

---
+++ DomainLayer/Entities/Result.cs
@@ -0,0 +1,24 @@
+﻿namespace InfotecsInternTask.DomainLayer.Entities;
+
+public partial class Result
+{
+    public int Resultid { get; set; }
+
+    public string Filename { get; set; } = null!;
+
+    public int Deltatime { get; set; }
+
+    public DateTime Mindate { get; set; }
+
+    public int Averageexecutiontime { get; set; }
+
+    public double Averagevalue { get; set; }
+
+    public double Medianvalue { get; set; }
+
+    public double Maxvalue { get; set; }
+
+    public double Minvalue { get; set; }
+
+    public virtual ICollection<Value> Values { get; set; } = new List<Value>();
+}
