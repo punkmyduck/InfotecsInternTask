083e19762d1fb51e0fc6754d63623dbf091d1422
punkmyduck <punkmyduck@mail.ru>
2025-08-11 10:56:18 +0300

    Вынос строки подключения в appsettings.json из dbContext'а

--- InfrastructureLayer/EfCoreDbContext/ProcessesdbContext.cs
+++ InfrastructureLayer/EfCoreDbContext/ProcessesdbContext.cs
@@ -18,10 +18,6 @@ public partial class ProcessesdbContext : DbContext
 
     public virtual DbSet<Value> Values { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=processesdb;Username=postgres;Password=123456");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Result>(entity =>
